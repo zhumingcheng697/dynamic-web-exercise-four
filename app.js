86ee0b7bb6759acda0a1f4ba9f7e11a51599753c
McCoy Zhu <mz1945@nyu.edu>
2020-11-05 11:41:46 -0800

    installed express and set up env

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.POST || 4000;
+const port = Number(process.env.POST) || 4000;
 
 const indexRoute = require("./routes/index");
 

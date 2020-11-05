811ec1b0d910c69309722c5c630d44a57d2cc6ad
McCoy Zhu <mz1945@nyu.edu>
2020-11-05 11:21:06 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.POST || 4000;
+
+const indexRoute = require("./routes/index");
+
+app.use("/", indexRoute);
+
+app.listen(port, () => {
+  console.log(`Exercise Four is running at localhost:${port}`);
+});

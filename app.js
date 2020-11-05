21f6891ceddfe7e7c4e54d16e21b37919fbb11c7
McCoy Zhu <mz1945@nyu.edu>
2020-11-05 12:39:08 -0800

    PORT not POST

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = Number(process.env.POST) || 4000;
+const port = Number(process.env.PORT) || 4000;
 
 const indexRoute = require("./routes/index");
 

2274e17fa1995e534d49c80087a98a96d061ce83
McCoy Zhu <mz1945@nyu.edu>
2020-11-12 12:00:11 -0800

    create form

--- routes/createBlogPost.js
+++ routes/createBlogPost.js
@@ -1,6 +1,37 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("Create Blog Post"));
+const firebase = require("firebase");
+
+const db = firebase.firestore();
+const blogPosts = db.collection("blogPosts");
+
+const form = `
+  <style>
+    body {
+      align-items: center;
+      display: flex;
+      justify-content: center;
+      min-height: 100vh;
+    }
+
+    form {
+      display: flex;
+      flex-direction: column;
+    }
+
+    form * {
+      margin: 5px 0;
+    }
+  </style>
+  <form>
+    <input type="text" name="title" placeholder="Title" />
+    <input type="text" name="author" placeholder="Author" />
+    <input type="text" name="body" placeholder="Body" />
+    <button type="submit">Submit Post</button>
+  </form>
+`;
+
+router.get("/", (req, res) => res.send(form));
 
 module.exports = router;
